1b626315002090b7ab4ac1bdd16c0e010722c568
Devious Minds <metroxynth@gmail.com>
2024-01-10 15:14:17 +0100

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bittensor-subnet27-startup
+
+git clone https://github.com/Metroxynth/bittensor-subnet27-startup
+cd bittensor-subnet27-startup
+sh or bash startup.sh
+
+And follow the required steps.
+
+KP
---
+++ startup.sh
@@ -0,0 +1,35 @@
+# Install Bittensor
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/opentensor/bittensor/master/scripts/install.sh)"
+
+# Clone compute-subnet repository
+git clone https://github.com/neuralinternet/compute-subnet.git
+cd compute-subnet
+
+# Install Python requirements
+python3 -m pip install -r requirements.txt
+
+# Install compute-subnet as a package
+python3 -m pip install -e .
+
+# Update and install npm
+sudo apt update
+sudo apt install npm -y
+
+# Install pm2 globally using npm
+sudo npm install pm2 -g
+
+# Install required packages for hashcat
+wget https://hashcat.net/files/hashcat-6.2.6.tar.gz
+tar xzvf hashcat-6.2.6.tar.gz
+cd hashcat-6.2.6/
+make
+make install  # prefixed by sudo if not in the sudoers
+hashcat --version
+
+# Allow specific ports through ufw
+sudo apt update
+sudo apt install ufw -y
+sudo ufw allow 12000:12001/tcp
+sudo ufw allow 22/tcp
+sudo ufw allow ssh
+sudo ufw enable
